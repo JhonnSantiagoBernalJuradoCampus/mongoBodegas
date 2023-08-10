1ae7a6c59a2845f7bc655671f7a41cb65f644e62
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-08-10 08:44:41 -0500

    feat: :tada: Configuracion basica express, db

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bodegas MongoDB
+## Diagrama base de datos
+Ejercicio de bodegas para practicar mongo, se creo la base de datos según este diagrama.
+
+<img src="./backend/assets/database.png">
---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const config = JSON.parse(process.env.MY_CONFIG);
+app.listen(config, ()=>{
+    console.log(`http://${config.hostname}:${config.port}`)
+})
