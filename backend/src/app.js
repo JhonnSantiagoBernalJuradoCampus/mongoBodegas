09f75fc6fa7d3fa7d9a356c479c690a1b866b943
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-08-11 16:33:59 -0500

    feat: :zap: Endpoint post inventario
    Si existe la combinacion se actualiza, si no se crea una nueva

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/users.js";
 import bodegasRouter from "./routes/bodegas.js";
 import productosRouter from "./routes/productos.js";
+import inventarioRouter from "./routes/inventarios.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use("/bodegas", bodegasRouter);
 app.use("/users", userRouter);
 app.use("/productos", productosRouter);
+app.use("/inventarios", inventarioRouter);
 
 const config = JSON.parse(process.env.MY_CONFIG);
 app.listen(config, ()=>{
---
+++ backend/src/routes/inventarios.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import { connectionDB } from "../../db/conexion.js";
+
+const router = Router();
+
+router.post("/add", async(req,res)=>{
+    try {
+        const db = await connectionDB();
+        const inventario = db.collection("inventarios");
+
+        const {id_producto, id_bodega, cantidad} = req.body;
+        const existe = await inventario.find({"id_producto": id_producto, "id_bodega": id_bodega}).toArray();
+        if(existe[0] === undefined){
+            inventario.insertOne({
+                    id_bodega: id_bodega,
+                    id_producto: id_producto,
+                    cantidad: cantidad,
+                    created_by: 25,
+                    created_at: null,
+                    updated_at: null,
+                    deleted_at: null
+            });
+            res.status(201).send({message: "Agregado con exito"})
+        }else {
+            const can = existe[0].cantidad;
+            const nueva = (cantidad+ can)
+            inventario.updateOne({"id_bodega": id_bodega,"id_producto": id_producto}, {$set: {cantidad: nueva}});
+            res.status(202).send({message: "Actualizado con exito"});
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({message: "Error en el servidor"});
+    }
+})
+
+export default router;
